219b58b59564a64f42159e738b83c0279e5389ee
dbarbour86 <derek.barbour@gmail.com>
2026-07-12 01:05:27 -0400

    feat: revert hardcoded Clarity script to env variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,15 +75,17 @@ export default function RootLayout({
         )}
 
         {/* Microsoft Clarity */}
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window,document,"clarity","script","xl4t4mu3o4");
-          `}
-        </Script>
+        {clarityId && (
+          <Script id="microsoft-clarity" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window,document,"clarity","script","${clarityId}");
+            `}
+          </Script>
+        )}
       </body>
     </html>
   );
